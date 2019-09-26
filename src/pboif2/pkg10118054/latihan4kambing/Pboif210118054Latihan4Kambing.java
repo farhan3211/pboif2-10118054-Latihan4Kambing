0dab079794f6b46ce383c874ee497853cbf213fc
farhan3211 <55830838+farhan3211@users.noreply.github.com>
2019-09-26 16:15:58 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118054/latihan4kambing/Pboif210118054Latihan4Kambing.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118054.latihan4kambing;
+
+/**
+ *
+ * @author ASUS
+ * nama  :Farhan Milardi
+ * kelas :IF-2
+ * nim   :10118054
+ */
+public class Pboif210118054Latihan4Kambing {
+
+    /**
+     * @param args the command line argumentsz
+     */
+public void tambahKambing() {
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        
+        System.out.println("Jumlah Kambing setelah ditambah: " +
+                         jumlahKambing);
+    }
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Pboif210118054Latihan4Kambing kambingJantan = new Pboif210118054Latihan4Kambing();
+        kambingJantan.tambahKambing();        
+
+    }
+    
+}
